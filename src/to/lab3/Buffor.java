9325d5a36383c5cbdd767fd3c233111c725e6bb8
Paweł Potaczała <potaczal@lab429-10.dydaktyka.icsr.agh.edu.pl>
2018-05-13 13:26:07 +0200

    add lab3

---
+++ src/to/lab3/Buffor.java
@@ -0,0 +1,37 @@
+package lab3;
+
+import java.util.LinkedList;
+
+public class Buffor {
+	
+	private LinkedList<Integer> linkedList = new LinkedList<Integer>();
+	private int capacity = 10;
+	
+	public int size() {
+		return linkedList.size();
+	}
+		   
+	public synchronized void put(int value) {
+		while (linkedList.size() >= capacity) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		linkedList.addFirst(value);
+		notifyAll();
+	}
+
+	public synchronized int get() {
+		while (linkedList.isEmpty()) {
+	         try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		int value = linkedList.removeLast();
+		notifyAll();
+		return value;
+	}
+
+}
